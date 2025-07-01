277b99e86b0e507a633a8c629374e021504256c5
Mars Kalender <mars@siyah.dev>
2025-07-01 20:36:18 +0300

    Nuget packages of validation

--- MiniCommerce.Api/Order/Validators/OrderCreateRequestValidator.cs
+++ MiniCommerce.Api/Order/Validators/OrderCreateRequestValidator.cs
@@ -1,6 +1,25 @@
-﻿namespace MiniCommerce.Api.Order.Validators;
+﻿using FluentValidation;
+using MiniCommerce.Application.Order.DTOs;
 
-public class OrderCreateRequestValidator
+namespace MiniCommerce.Api.Order.Validators;
+
+public class OrderCreateRequestValidator : AbstractValidator<OrderCreateRequest>
 {
-    
+    public OrderCreateRequestValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("UserId is required.");
+
+        RuleFor(x => x.ProductId)
+            .NotEmpty().WithMessage("ProductId is required.");
+
+        RuleFor(x => x.Quantity)
+            .NotEmpty().WithMessage("Quantity is required.")
+            .GreaterThan(0).WithMessage("Quantity must be more than 0.")
+            .LessThanOrEqualTo(1000).WithMessage("Quantity must not exceed 1000.");
+
+        RuleFor(x => x.PaymentMethod)
+            .NotNull().WithMessage("PaymentMethod is required.")
+            .IsInEnum().WithMessage("An invalid value was provided for PaymentMethod.");
+    }
 }
